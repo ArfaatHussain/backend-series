ee86bdb6d58b1c587371e25b6a27dbccc9b363ef
ArfaatHussain <arfaathussain745@gmail.com>
2025-07-11 22:10:52 +0500

    Added User, Video model with hooks and authentication methods

---
+++ src/models/video.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    videoFile: String,
+    thumbnail: String,
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: String,
+    description: String,
+    duration: {
+        type: Number,
+        default: 0
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    comments: {
+        type: Number,
+        default: 0
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    }
+}, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video",videoSchema)
